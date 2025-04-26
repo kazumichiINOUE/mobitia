8ed571f3c226b0d400267c09b35290ae8a41f463
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2025-04-26 19:02:14 +0900

    refactor: 名前の短縮とプロセス停止のデモ

--- main.cpp
+++ main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
+
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
+using LockGuard = std::lock_guard<std::mutex>;
 
 std::mutex mtx; 
+bool running = true;
 
 void thread_motor(int& shared_data) {
   std::cout << "Motor thread started." << std::endl;
   for (int i = 0; i < 10000; ++i) {
-    std::lock_guard<std::mutex> lock(mtx); 
+    LockGuard lock(mtx); 
     shared_data++;
+  }
+  while (running) {
+    sleep_for(milliseconds(500));
   }
 }
 
 void thread_lidar(int& shared_data) {
   std::cout << "LiDAR thread started." << std::endl;
   for (int i = 0; i < 10000; ++i) {
-    std::lock_guard<std::mutex> lock(mtx);
+    LockGuard lock(mtx);
     shared_data++;
+  }
+  while (running) {
+    sleep_for(milliseconds(500));
   }
 }
 
@@ -25,6 +38,9 @@ int main(int argc, char* argv[]) {
 
   std::thread th_motor(thread_motor, std::ref(shared_data));
   std::thread th_lidar(thread_lidar, std::ref(shared_data));
+
+  sleep_for(seconds(3));   // 3秒待ってからスレッドを停止する
+  running = false;          // threadを停止
 
   th_motor.join();
   th_lidar.join();
