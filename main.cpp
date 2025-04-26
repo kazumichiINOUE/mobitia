c4b39429efba3dbc7004d53def71f564f2b3c9a9
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2025-04-26 20:27:06 +0900

    refactor: スマートポインタを使ってコード整理

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <memory>
 
 using std::chrono::milliseconds;
 using std::chrono::seconds;
@@ -11,42 +12,81 @@ using LockGuard = std::lock_guard<std::mutex>;
 std::mutex mtx; 
 bool running = true;
 
-void thread_motor(int& shared_data) {
-  std::cout << "Motor thread started." << std::endl;
+struct STATUS {
+  double x, y, a;
+  double cmd_v, cmd_w;
+
+  STATUS() {
+    x = 0.0;
+    y = 0.0;
+    a = 0.0;
+    cmd_v = 0.0;
+    cmd_w = 0.0;
+  };
+};
+
+struct ODOMETRY {
+  double x, y, a;
+
+  ODOMETRY() {
+    x = 0.0;
+    y = 0.0;
+    a = 0.0;
+  };
+};
+
+void thread_motor(std::shared_ptr<STATUS> st, std::shared_ptr<ODOMETRY> odo) {
+  // スレッド起動メッセージ
+  {
+    LockGuard lock(mtx);
+    std::cout << "Motor thread started." << std::endl;
+  }
+  // 何かしらの処理
   for (int i = 0; i < 10000; ++i) {
     LockGuard lock(mtx); 
-    shared_data++;
+    st->x += 1.0;
   }
+  // 待機
   while (running) {
     sleep_for(milliseconds(500));
   }
 }
 
-void thread_lidar(int& shared_data) {
-  std::cout << "LiDAR thread started." << std::endl;
+void thread_lidar(std::shared_ptr<STATUS> st) {
+  // スレッド起動メッセージ
+  {
+    LockGuard lock(mtx);
+    std::cout << "LiDAR thread started." << std::endl;
+  }
+  // 何かしらの処理
   for (int i = 0; i < 10000; ++i) {
     LockGuard lock(mtx);
-    shared_data++;
+    st->x += 1.0;
   }
+  // 待機
   while (running) {
     sleep_for(milliseconds(500));
   }
 }
 
 int main(int argc, char* argv[]) {
-  int shared_data = 0;
+  // 共有オブジェクト
+  auto state = std::make_shared<STATUS>();
+  auto odo = std::make_shared<ODOMETRY>();
 
-  std::thread th_motor(thread_motor, std::ref(shared_data));
-  std::thread th_lidar(thread_lidar, std::ref(shared_data));
+  // スレッド起動
+  std::thread th_motor(thread_motor, state, odo);
+  std::thread th_lidar(thread_lidar, state);
 
-  sleep_for(seconds(3));   // 3秒待ってからスレッドを停止する
+  sleep_for(seconds(1));    // 1秒待ってからスレッドを停止する
   running = false;          // threadを停止
 
+  // スレッドの終了待機
   th_motor.join();
   th_lidar.join();
 
-  std::cout << "shared_data = " << shared_data << std::endl;
-
+  // 終了処理
+  std::cout << "shared_data = " << state->x << std::endl;
   std::cout << "Bye." << std::endl;
   return 0;
 }
