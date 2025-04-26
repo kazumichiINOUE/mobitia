ef44c331dba5bf9fa765d161766f0a0aea03957c
KazumichiINOUE <kazumichiinoue@mail.saitama-u.ac.jp>
2025-04-26 18:37:59 +0900

    refactor: thread名を修正

--- main.cpp
+++ main.cpp
@@ -23,11 +23,11 @@ void thread_lidar(int& shared_data) {
 int main(int argc, char* argv[]) {
   int shared_data = 0;
 
-  std::thread t1(thread_motor, std::ref(shared_data));
-  std::thread t2(thread_lidar, std::ref(shared_data));
+  std::thread th_motor(thread_motor, std::ref(shared_data));
+  std::thread th_lidar(thread_lidar, std::ref(shared_data));
 
-  t1.join();
-  t2.join();
+  th_motor.join();
+  th_lidar.join();
 
   std::cout << "shared_data = " << shared_data << std::endl;
 
